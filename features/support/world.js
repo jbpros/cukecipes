2c45a304d6a149cceb4d986802e79c606f395918
Julien Biezemans <jb@jbpros.com>
2012-03-27 16:33:27 +0200

    Simplify World require path

--- features/support/world.js
+++ features/support/world.js
@@ -1,4 +1,4 @@
 module.exports = function () {
   var worldType = process.env.WORLD_TYPE || 'persistence';
-  this.World = require('../support/' + worldType + '_world').World;
+  this.World = require('./' + worldType + '_world').World;
 };
